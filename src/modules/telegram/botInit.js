10a7dbc85fc6127dd98c75d563a04784c370e0b2
Pham Duc Vuong <ducvuongpham2004@outlook.com>
2022-07-09 08:44:40 +0700

    feat: add botinit

---
+++ src/modules/telegram/botInit.js
@@ -0,0 +1,25 @@
+require('dotenv').config({ path: '../../.env' });
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.TELEGRAM_API_KEY;
+const bot = new TelegramBot(token, { polling: true });
+bot.on('polling_error', (err) => console.log(err));
+
+bot.editInlineText = (text, message) => {
+  if (message.text === text) return;
+  bot.editMessageText(text, {
+    chat_id: message.chat.id,
+    message_id: message.message_id,
+    reply_markup: message.reply_markup,
+  });
+};
+
+bot.switchMenu = (text, message, keyboard) => {
+  bot.editMessageText(text, {
+    chat_id: message.chat.id,
+    message_id: message.message_id,
+    reply_markup: keyboard,
+  });
+};
+
+module.exports = { bot };
